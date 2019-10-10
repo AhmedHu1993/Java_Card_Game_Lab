773271743ec63ec5cd56c9448ba70d05af72fa3f
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-10 13:52:42 +0100

    player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Player {
+
+    private ArrayList<Card> hand;
+
+    public Player(){
+        this.hand = new ArrayList<Card>();
+    }
+
+    public ArrayList getHand(){
+        return this.hand;
+    }
+
+    public void addCardToHand(Card card){
+        this.hand.add(card);
+    }
+}
