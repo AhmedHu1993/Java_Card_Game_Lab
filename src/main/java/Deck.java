b2116d32cc85d4ed552b1ebae93410d4f1d30e46
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-10-10 11:49:15 +0100

    Deck class and TDD

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> deckOfCards;
+
+    public Deck(){
+        this.deckOfCards = new ArrayList<Card>();
+    }
+
+    public int getDeckOfCardsCount(){
+        return this.deckOfCards.size();
+    }
 }
